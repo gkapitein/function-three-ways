e2d7a36f9d51e9bf48bab42eba66e30e53caf6d1
gkapitein <84490555+gkapitein@users.noreply.github.com>
2021-06-11 08:28:03 +0200

    Delete opdracht arguments directory

--- opdracht arguments/script.js
+++
@@ -1,7 +0,0 @@
-const paintWall = function (colora, colorb) {
-    console.log("the wall has been painted" + colora);
-    console.log("The north wall has been painted" + colorb);
-}
-
-paintWall("red", "green");
-
